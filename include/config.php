88a4b6a62d822739cf7144379d1e06f9ab269260
EC2 Default User <ec2-user@ip-172-30-4-196.ec2.internal>
2020-02-15 18:02:32 +0000

    Initial

---
+++ include/config.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * Son of Service
+ * Copyright (C) 2003-2009 by Andrew Ziem.  All rights reserved.
+ * Licensed under the GNU General Public License.  See COPYING for details.
+ *
+ * $Id: config.php,v 1.12 2009/02/12 04:11:20 andrewziem Exp $
+ *
+ */
+
+if (preg_match('/config.php/i', $_SERVER['PHP_SELF']))
+{
+    die('Do not access this page directly.');
+}
+
+//
+//  TO CONFIGURE
+//    This configuration file is in PHP syntax.  To configure,
+//    edit the values on the right side of the equals sign.
+//    To change a string, enclose the characters inside
+//    the quotation marks.
+
+$cfg['ado_path'] = SOS_PATH . 'adodb';
+
+$cfg['dbtype'] = 'mysql';	//default: mysql
+$cfg['dbhost'] = 'database-1.cbkwsq59mx5a.us-east-1.rds.amazonaws.com';
+$cfg['dbpass'] = 'lDWUp2cbP3ub6FMIHZYf';
+$cfg['dbuser'] = 'admin';
+$cfg['dbname'] = 'sos';
+$cfg['dbpersist'] = FALSE;	//default: persistant connections on (true)
+
+$smtp_hostname = 'localhost';
+
+// default language (for cases where language cannot be detected)
+//  examples: en_US, nl_NL, es_BR
+$default_language = 'en_US';
+$default_language = 'nl_NL';
+
+
+// html_security_level
+// 1: HTML stripped from most form input for maximum security
+// 2: admin-level users can include some HTML
+// 3: all users can include some HTML
+$html_security_level = 1;
+
+// db_cache_timeout: measured in seconds
+$db_cache_timeout = 60;
+
+$ADODB_CACHE_DIR = SOS_PATH . 'data/';
+
+$debug = TRUE;
+
+?>
