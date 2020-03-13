0ba40fdad00f68f16ea4feea4ddb4d8fa65b4589
dejongmit <dejongmi@mail.gvsu.edu>
2020-03-13 15:08:18 -0400

    fourth time???

--- src/add_volunteer.php
+++ src/add_volunteer.php
@@ -39,70 +39,71 @@ function test_input($data) {
 function volunteer_add()
 {
     global $db;
-    
-    
+
+
     // validate form input
 
     $errors_found = 0;
-    
+
     if (2 > (strlen(trim($_POST['last'])) + strlen(trim($_POST['organization']))))
     {
        process_user_error(_("Please enter a longer last name or organization."));
        $errors_found++;
     }
-    
+
     if (!has_permission(PC_VOLUNTEER, PT_WRITE, NULL, NULL))
     {
 	process_user_error(_("Insufficient permissions."));
-	$errors_found++;	
-    }    
-
-    if ($errors_found)
-    {    
-	  echo ("<P>Try <A href=\"add_volunteer.php\">again</A>.</P>\n");
-	  // todo: redisplay form here with values in place
-	  die();
+	$errors_found++;
     }
-        
+
     $organization = $db->qstr(htmlentities($_POST['organization']), get_magic_quotes_gpc());
 
-    $prefix = $db->qstr(htmlentities($_POST['prefix']), get_magic_quotes_gpc()); 
+    $prefix = $db->qstr(htmlentities($_POST['prefix']), get_magic_quotes_gpc());
     //$first = $db->qstr(htmlentities($_POST['first']), get_magic_quotes_gpc());
 	$firstErr = "";
 	if (empty($_POST["first"])) {
 		$firstErr = "First Name is Required";
+		$errors_found++;
 	} else {
 		$first = $db->test_input($_POST['first']);
 	}
-	
-    $middle = $db->qstr(htmlentities($_POST['middle']), get_magic_quotes_gpc());      
-    $last = $db->qstr(htmlentities($_POST['last']), get_magic_quotes_gpc());      
-   
-    $street = $db->qstr(htmlentities($_POST['street']), get_magic_quotes_gpc());         
-    $city = $db->qstr(htmlentities($_POST['city']), get_magic_quotes_gpc());            
+
+    $middle = $db->qstr(htmlentities($_POST['middle']), get_magic_quotes_gpc());
+    $last = $db->qstr(htmlentities($_POST['last']), get_magic_quotes_gpc());
+
+    $street = $db->qstr(htmlentities($_POST['street']), get_magic_quotes_gpc());
+    $city = $db->qstr(htmlentities($_POST['city']), get_magic_quotes_gpc());
     $state = $db->qstr(htmlentities($_POST['state']), get_magic_quotes_gpc());
-    $postal_code = $db->qstr(htmlentities($_POST['postal_code']), get_magic_quotes_gpc());   
-    $country = $db->qstr(htmlentities($_POST['country']), get_magic_quotes_gpc());       
-   
-    $email_address = $db->qstr(htmlentities($_POST['email_address']), get_magic_quotes_gpc());      
-   
+    $postal_code = $db->qstr(htmlentities($_POST['postal_code']), get_magic_quotes_gpc());
+    $country = $db->qstr(htmlentities($_POST['country']), get_magic_quotes_gpc());
+
+    $email_address = $db->qstr(htmlentities($_POST['email_address']), get_magic_quotes_gpc());
+
+		if ($errors_found) {
+		  echo ("<P>Try <A href=\"add_volunteer.php\">again</A>.</P>\n");
+		  // todo: redisplay form here with values in place
+		  die();
+    }
+
+
     $sql = 'INSERT INTO volunteers '.
 	    '(prefix, first,middle,last,organization,street,city,state,postal_code,country,email_address, dt_added, uid_added, dt_modified, uid_modified) '.
 	    "VALUES ($prefix, $first, $middle, $last, $organization, $street, $city, $state, $postal_code, $country, $email_address, now(), ".get_user_id().", now(), uid_added)";
 
     $result = $db->Execute($sql);
 
-    if (!$result) 
-    { 
+    if (!$result)
+    {
 	die_message(MSG_SYSTEM_ERROR, _("Error adding data to database."), __FILE__, __LINE__, $sql);
     }
-    
+
     $vid = $db->Insert_ID();
-    
+
     // insert phone number records
-    
+
     if (!empty($_POST['phone_home']) or !empty($_POST['phone_work']) or !empty($_POST['phone_cell']))
-    {	
+    {
 	// select an empty record
 
 	$sql = "SELECT * FROM phone_numbers WHERE 0 = 1";
@@ -111,12 +112,12 @@ function volunteer_add()
 	{
 	    die_message(MSG_SYSTEM_ERROR, _("Error querying database."), __FILE__, __LINE__, $sql);
 	}
-	
+
 	$record['volunteer_id'] = $vid;
     }
 
     if (!empty($_POST['phone_home']))
-    {	
+    {
 	$record['number'] =  htmlentities($_POST['phone_home']);
 	$record['memo'] = _("Home");
 	$sql = $db->GetInsertSql($template_result, $record);
@@ -129,35 +130,35 @@ function volunteer_add()
     }
 
     if (!empty($_POST['phone_work']))
-    {	    	
+    {
 	$record['number'] =  htmlentities($_POST['phone_work']);
 	$record['memo'] = _("Work");
 	$sql = $db->GetInsertSql($template_result, $record);
 	$result = $db->Execute($sql);
 	if (!$result)
 	{
-	    // todo: roll back	
+	    // todo: roll back
 	    die_message(MSG_SYSTEM_ERROR, _("Error adding data to database."), __FILE__, __LINE__, $sql);
 	}
-    }	
+    }
 
     if (!empty($_POST['phone_cell']))
-    {	    	    
+    {
 	$record['number'] =  htmlentities($_POST['phone_cell']);
 	$record['memo'] = _("Cell");
 	$sql = $db->GetInsertSql($template_result, $record);
 	$result = $db->Execute($sql);
 	if (!$result)
 	{
-	    // todo: roll back	
+	    // todo: roll back
 	    die_message(MSG_SYSTEM_ERROR, _("Error adding data to database."), __FILE__, __LINE__, $sql);
-	}	
+	}
     }
-     
+
     // display success message
-    
+
     $volunteer_row = volunteer_get($vid, $errstr);
-    
+
     if ($volunteer_row)
     {
         echo ("<P>" . _("Added:") . " <A href=\"". SOS_PATH . "volunteer/?vid=$vid\">" . make_volunteer_name($volunteer_row) . ' (#'.$vid.")</A>.</P>\n");
@@ -173,8 +174,8 @@ function volunteer_add()
 
 function volunteer_add_form()
 {
-    
-?>    
+
+?>
     <form method="post" action="add_volunteer.php">
 
 <table border="0" width="50%" cellspacing="0" cellpadding="0">
@@ -197,7 +198,7 @@ function volunteer_add_form()
 <tr>
  <th class="vert"><?php echo _("Organization"); ?></th>
  <td><input type="text" name="organization"></td>
- </tr> 
+ </tr>
 <tr>
  <th class="vert"><?php echo _("Street"); ?></th>
  <td><input type="Text" name="street"></td>
@@ -244,23 +245,23 @@ function volunteer_add_form()
 
 } /* volunteer_add_form() */
 
-if (array_key_exists('button_add_volunteer', $_POST)) 
+if (array_key_exists('button_add_volunteer', $_POST))
 {
     $db = connect_db();
 
     if (!$db)
     {
-        die_message(MSG_SYSTEM_ERROR, _("Error establishing database connection."), __FILE__, __LINE__);	
+        die_message(MSG_SYSTEM_ERROR, _("Error establishing database connection."), __FILE__, __LINE__);
     }
 
     volunteer_add();
 }
-else 
+else
 {
-    volunteer_add_form();  
+    volunteer_add_form();
 }
-  
-  
+
+
 
 make_html_end();
 
