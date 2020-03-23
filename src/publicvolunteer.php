5154c908e7dd97582f81b656d5f09aac383f1f14
bdb325 <bdb325@ymail.com>
2020-03-23 17:23:50 -0400

    testing 2

--- src/publicvolunteer.php
+++ src/publicvolunteer.php
@@ -85,7 +85,7 @@ function volunteer_add()
 
     $sql = 'INSERT INTO volunteers '.
 	    '(f_name,m_initial,l_name,race,ethnicity,gender,veteran_status,volunteer_type,refered_from,birth_date,email_address,phone_number,country,street_address,state_providence,city,postal_code,emergency_fName,emergency_lName,emergency_relationship) '.
-	    "VALUES ($first, $middle, $last, $race, $ethnicity, $gender, $veteran_status, $volunteer_type, $refered_from, $birth_date, $email_address, $phone_number, 'US', $street_address, 'MI', 'Marysville', $postal_code, $emergency_fname, $emergency_lname, $emergency_relationship)";
+	    "VALUES ($first, $middle, $last, $race, $ethnicity, $gender, $veteran_status, $volunteer_type, $refered_from, $birth_date, $email_address, $phone_number, 'US', $street, 'MI', 'Marysville', $postal_code, $emergency_fname, $emergency_lname, $emergency_relationship)";
 
     $result = $db->Execute($sql);
 
