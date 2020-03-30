5711acefd9b9d8ea90c2abfbe4bfe23fdf2a69e0
bdb325 <bdb325@ymail.com>
2020-03-30 15:41:07 -0400

    changed references to $stmt

--- src/search_volunteer.php
+++ src/search_volunteer.php
@@ -334,7 +334,7 @@ function volunteer_search_display($sql, $offset, $results_per_page)
 	// search successful
 	// todo: mass-action on found set (email)
 
-        if (0 == ($total_results = $result->numRows))
+        if (0 == ($total_results = $stmt->numRows))
         {
              process_user_error(_("Found zero volunteers matching your description."));
         }
