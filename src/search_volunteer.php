f4a69002214be2db898843c7b569677912df9027
bdb325 <bdb325@ymail.com>
2020-03-30 16:07:01 -0400

    function deprecation

--- src/search_volunteer.php
+++ src/search_volunteer.php
@@ -354,7 +354,7 @@ function volunteer_search_display($sql, $offset, $results_per_page)
 
 	    $fieldnames = array();
 
-	    for ($i = 0, $max = $result->field_count(); $i < $max; $i++)
+	    for ($i = 0, $max = $result->field_count; $i < $max; $i++)
 	    {
 		$fld = $result->fetch_field($i);
 		$fieldnames[$fld->name] = array();
