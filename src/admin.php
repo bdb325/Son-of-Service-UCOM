7f40333c5428b376eeeed522a3c0b84144432592
bdb325 <bdb325@ymail.com>
2020-04-01 15:32:35 -0400

    php makes no sense sometimes

--- src/admin.php
+++ src/admin.php
@@ -118,7 +118,7 @@ if (isset($_POST['searchUpdate'])) {
   $stmt = $db->prepare($sql);
   $stmt->bind_param("sss", $first, $last, $email);
   $stmt->execute();
-  $data = $stmt->fetch_array(MYSQLI_BOTH);
+  $data = $stmt->fetch_all(MYSQLI_BOTH);
   echo($data[0]);
 
 
