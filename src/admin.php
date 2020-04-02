b29027ea22adbd1788e51cd13a6434cf615ec45e
bdb325 <bdb325@ymail.com>
2020-04-02 19:16:37 -0400

    horse tranquilzer

--- src/admin.php
+++ src/admin.php
@@ -146,6 +146,7 @@ if (isset($_POST['searchUpdate'])) {
     $var14 = $dataset[13];
     $var15 = $dataset[14];
 }
+  print_r($dataset);
   updateVolunteerForm($var1,$var2,$var3,$var4,$var5,$var6,$var7,$var8,$var9,$var10,$var11,$var12,$var13,$var14,$var15);
 
 
