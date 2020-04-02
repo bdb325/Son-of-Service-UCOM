c11f11ef1e70d74d6214a0b6213c3c70fc06768d
bdb325 <bdb325@ymail.com>
2020-04-02 19:44:27 -0400

    Integrated PHP search function that displays results in HTML table.

--- src/admin.php
+++ src/admin.php
@@ -54,7 +54,7 @@ function results() {
 
 
 /* Displays Volunteer Info for updating after seaching. I'm so sorry I passed in 15 variables :/ */
-function updateVolunteerForm($var1,$var2,$var3,$var4,$var5,$var6,$var7,$var8,$var9,$var10,$var11,$var12,$var13,$var14,$var15) {
+function updateVolunteerForm($var1,$var2,$var3,$var4,$var5,$var6,$var7,$var8,$var9,$var10,$var11,$var12,$var13,$var14,$var15,$var16) {
 
 ?>
 
