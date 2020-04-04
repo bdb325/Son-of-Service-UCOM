7e232b2dba163375dce521b6595f810fbfe45d2b
bdb325 <bdb325@ymail.com>
2020-04-04 15:08:07 -0400

    testing again

--- src/admin.php
+++ src/admin.php
@@ -192,11 +192,11 @@ if (isset($_POST['searchUpdate'])) {
    $emergency_lname = $db->real_escape_string($_POST['emergency_lName']);
    $emergency_phone = $db->real_escape_string($_POST['emergency_phone']);
    $emergency_relationship = $db->real_escape_string($_POST['emergency_relationship']);
-   $sql = "UPDATE VOLUNTEER V SET V.f_name = ?, V.m_initial = ?, V.l_name = ?, V.race = ?,
-   V.ethnicity = ?, V.gender = ?, V.veteran_status = ?, V.birth_date = ?, V.email_address = ?,
-   V.phone_number = ?, V.country = ?, V.street_address = ?, V.state_providence = ?, V.city = ?, V.postal_code = ?,
-  V.emergency_fName = ?, V.emergency_lName = ?, V.emergency_phone = ?, V.emergency_relationship = ? WHERE V.f_name = ?
-   AND V.l_name = ? AND V.email_address = ?";
+   $sql = "UPDATE VOLUNTEER SET f_name = ?, m_initial = ?, l_name = ?, race = ?,
+   ethnicity = ?, gender = ?, veteran_status = ?, birth_date = ?, email_address = ?,
+   phone_number = ?, country = ?, street_address = ?, state_providence = ?, city = ?, postal_code = ?,
+  emergency_fName = ?, emergency_lName = ?, emergency_phone = ?, emergency_relationship = ? WHERE f_name = ?
+   AND l_name = ? AND email_address = ?";
    if($stmt = $db->prepare($sql)) {
      //19 values are prepared
      $stmt->bind_param("ssssssssssdssssssssssss", $first, $middle, $last, $race, $ethnicity, $gender, $veteran_status, $volunteer_type, $birth_date,
