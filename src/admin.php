5725899d4310db0e11a05dbc60b0edcdfddfa98e
bdb325 <bdb325@ymail.com>
2020-04-07 19:44:38 -0400

    I JUST WANT IT TO WORK

--- src/admin.php
+++ src/admin.php
@@ -146,6 +146,7 @@ if (isset($_POST['searchUpdate'])) {
   updateVolunteerForm($var1,$var2,$var3,$var4,$var5,$var6,$var7,$var8,$var9,$var10,$var11,$var12,$var13,$var14,$var15,$var16,$var17,$var18,$var19,$var20);
   if (isset($_POST['update'])) {
   /*  $stmt = $db->prepare($sql); */
+  global $db;
       $first = $db->real_escape_string($_POST['firstna']);
       $indexFirst = $db->real_escape_string($_POST['firstna']);
       $middle = $db->real_escape_string($_POST['minit']);
