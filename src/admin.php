796464f2d84c15ffd16960b023ebf61ff97060b3
bdb325 <bdb325@ymail.com>
2020-04-07 20:32:17 -0400

    ADMIN

--- src/admin.php
+++ src/admin.php
@@ -144,6 +144,7 @@ if (isset($_POST['searchUpdate'])) {
     $var20 = $dataset['emergency_relationship'];
    }
   updateVolunteerForm($var1,$var2,$var3,$var4,$var5,$var6,$var7,$var8,$var9,$var10,$var11,$var12,$var13,$var14,$var15,$var16,$var17,$var18,$var19,$var20);
+}
   if (isset($_POST['update'])) {
   /*  $stmt = $db->prepare($sql); */
   global $db;
@@ -197,7 +198,7 @@ if (isset($_POST['searchUpdate'])) {
    */
 
  }
-  }
+
 
 
 
